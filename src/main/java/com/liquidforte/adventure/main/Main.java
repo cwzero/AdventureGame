89d8ed754aaafed55327808488d09f46fbdc36c2
Eric Gjerde <gjerdeeric@gmail.com>
2020-01-21 10:18:46 -0600

    Add Notes

---
+++ src/main/java/com/liquidforte/adventure/main/Main.java
@@ -0,0 +1,77 @@
+package com.liquidforte.adventure.main;
+
+public class Main {
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * Thematic: Dark, Secretive
+		 * 	Modern day, truth hidden
+		 * 
+		 */
+		
+		/*
+		 * 	Goals - 
+		 * 		Reacts to the player
+		 *		Dynamic so that it's replayable
+		 *		Easy to learn but unique
+		 *		Not predictable - even if you know how it works, it's still fun
+		 *		Intelligent - feels like you're playing with people
+		 *		Infinite - you can do anything, anything can happen
+		 *		Hidden secrets - clues integrated with story
+		 *		Storyline - mystery to solve
+		 */
+		
+		/*
+		 *	TODO: Player Control
+		 *
+		 *	TODO: Dialogue System
+		 *
+		 * 	TODO: Rumor System (Secrets)
+		 * 
+		 *	TODO: Action System
+		 *		How to model a dynamic world, with infinite options - how to be more than a choose-your-own-adventure
+		 *		Like a physics system for abstract and social stuff
+		 *
+		 *	TODO: Inventory System
+		 *
+		 *	TODO: Loot System
+		 *
+		 *	TODO: Legacy Item System
+		 *		Items which have living traits, haunted and intelligent.  Some may be sentient and able to take human form
+		 *
+		 *	TODO: Storyline - Diverging paths, Generation of Possible events, tracking future timelines
+		 *
+		 *	TODO: Event System
+		 *		Storyline needs to be dynamic, events are not random but more than choose-your-own adventure
+		 *		Can't think of everything, need a model and system to generate different events for replayability
+		 *		Reactive - player actions affect the future
+		 *
+		 *	TODO: Destiny System
+		 *		Weighted future events - push toward or away from certain paths in the future
+		 *		This might be determined in the background, affects the future of the storyline and of characters
+		 *		Integrate with secrets - reveal which characters have secrets
+		 *
+		 *	TODO: Factions
+		 *		Like characters, act with intention and have infinite options
+		 *		But they're factions composed of multiple characters and subgroups
+		 *
+		 *	TODO: Reputation System
+		 *
+		 *	TODO: Goal Orientation System
+		 *		Have the relevant NPCs work toward a goal.  
+		 *		How do we make them able to act dynamically with infinite options?  
+		 *		How to make them realistic i.g. they don't always think of every option
+		 *
+		 *	TODO: NPC Reaction system
+		 *
+		 *	TODO: Character interaction
+		 *
+		 *	TODO: Monsters?
+		 *
+		 *	TODO: Dungeon equivalent?
+		 *
+		 *	TODO: Magic?
+		 * 
+		 */
+	}
+}
